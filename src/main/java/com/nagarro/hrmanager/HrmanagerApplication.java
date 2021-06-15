3d9a4228849f567a03100de278c8c6f412c3fd82
aryantyagi <aryan.tyagi@nagarro.com>
2021-06-15 17:59:10 +0530

    Some Changes in Assignment - HR Manager System with spring Boot and RESTful webservices

--- src/main/java/com/nagarro/hrmanager/HrmanagerApplication.java
+++ src/main/java/com/nagarro/hrmanager/HrmanagerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HrmanagerApplication {
 
 	public static void main(String[] args) {
+		//To run the my spring boot application
 		SpringApplication.run(HrmanagerApplication.class, args);
+		System.out.println("Hello, Welcome to my application");
 		
 	}
 
